99a28aca9f9db6b53ebf390e756c74def068e878
KateRom98 <106525538+KateRom98@users.noreply.github.com>
2023-03-13 21:04:29 +0100

    Created a core of a weather website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="Weather">Weather React App</h1>
+      <div className="container">
+        <h1 className="text-center m-5">Weather</h1>
+        <Weather />
+        <footer>
+          This website was built by{" "}
+          <a href="https://magenta-pika-c7a68c.netlify.app/">
+            {" "}
+            Kateryna Dibtsova
+          </a>{" "}
+          and is open-sourced on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input type="search" placeholder="Enter a city..." />
+        <input type="submit" value="Search" />
+      </form>
+      <h3 className="text-center m-4 fs-1">Berlin, Germany</h3>
+      <div className="row weatherIndex m-0">
+        <div className="col text-center">
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAg5JREFUeNrt2tGRgyAQBmBLoARLSAmUkBIsISVYgiVYgiVQgq//myXQAfdwS4bJqRFuCWjWmZ3JaGYin8AumMY513xzNAIgAAIgAAIgAAIgAAIgAALwe4LhANACGADMANxLzHStbQoc2QGoce5g9JcBAKA2nvi7mAGoUwP8o/FPhOoBAGgAIwBD0ftxHNntiw6HJICdBiqa8BxD2E9MjNEAAO4bNzwxPn0fA/WsbNkjBWDZuNkHXZ8ZAQ5DfQQAwG3nJjR9xxWKpOwRC6D3AGgOcGdCYAMI0p8rHEMWAGq82Zit++B7roJoWQEAdBs/NPouR/PDUAnAwAaw0+27YKEzVdLw6EryCIDZabymIeBqC06AP93+QEa4LMASlLv2GwF81zc1N54bwJe29kA1WHMY/wBjAe4vY388KUBYLbY5FkNniudSOwXAXSTmlLWAKtRl/a4T91K7qxlgXKvpKQVzzUOmxiFgg70FBeBBu0I9fVacVWiNk6AOFmB2A6gLUrIO9wAoa5mcADnT4Phm9blWlClC0B6Pzve5AHIWQm1EmW1pSNiVcv129GGlboubXGmJGsWS56l37GFOqQA5FkMmputGDKeRLQ1+4mAEWKp4OxzR6KXEirA4QOZ9RPPuHWNRAMb3iMmbpKUBdOkNktIAt+oB5F9iAiAAAiAAAiAAAiAAAiAAAiAAAiAAl48fFVnRpiVnD+AAAAAASUVORK5CYII="
+            alt="Clouds"
+            width="150px"
+          />
+          <div>Cloudy</div>
+        </div>
+        <div className="col temperature">14°C</div>
+        <div className="col align-self-center mt-2">
+          <ul>
+            <li>Wind: 21 km/h</li>
+            <li>Humidity: 74%</li>
+            <li>Pressure: 1025.0 mb</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
