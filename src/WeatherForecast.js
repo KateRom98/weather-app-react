5b86a38505677daf7fd4f4c64ea43b7cb505e5f6
KateRom98 <106525538+KateRom98@users.noreply.github.com>
2023-03-22 22:45:37 +0100

    Implemented the first day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,26 +1,31 @@
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-  function handleResponse(response) {}
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.daily[1]);
+    setLoaded(true);
+  }
 
-  let apiKey = `90a3822d88740dfa4e667ccot8cebf7b`;
-  let queryCity = props.query;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${queryCity}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAg5JREFUeNrt2tGRgyAQBmBLoARLSAmUkBIsISVYgiVYgiVQgq//myXQAfdwS4bJqRFuCWjWmZ3JaGYin8AumMY513xzNAIgAAIgAAIgAAIgAAIgAALwe4LhANACGADMANxLzHStbQoc2QGoce5g9JcBAKA2nvi7mAGoUwP8o/FPhOoBAGgAIwBD0ftxHNntiw6HJICdBiqa8BxD2E9MjNEAAO4bNzwxPn0fA/WsbNkjBWDZuNkHXZ8ZAQ5DfQQAwG3nJjR9xxWKpOwRC6D3AGgOcGdCYAMI0p8rHEMWAGq82Zit++B7roJoWQEAdBs/NPouR/PDUAnAwAaw0+27YKEzVdLw6EryCIDZabymIeBqC06AP93+QEa4LMASlLv2GwF81zc1N54bwJe29kA1WHMY/wBjAe4vY388KUBYLbY5FkNniudSOwXAXSTmlLWAKtRl/a4T91K7qxlgXKvpKQVzzUOmxiFgg70FBeBBu0I9fVacVWiNk6AOFmB2A6gLUrIO9wAoa5mcADnT4Phm9blWlClC0B6Pzve5AHIWQm1EmW1pSNiVcv129GGlboubXGmJGsWS56l37GFOqQA5FkMmputGDKeRLQ1+4mAEWKp4OxzR6KXEirA4QOZ9RPPuHWNRAMb3iMmbpKUBdOkNktIAt+oB5F9iAiAAAiAAAiAAAiAAAiAAAiAAAiAAl48fFVnRpiVnD+AAAAAASUVORK5CYII=" />
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">19°</span>
-            <span className="WeatherForecast-temp-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `90a3822d88740dfa4e667ccot8cebf7b`;
+    let queryCity = props.query;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${queryCity}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,32 @@
+export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = date.getDay();
+    return days[day];
+  }
+
+  function max() {
+    let maxTemp = Math.round(props.data.temperature.maximum);
+    return `${maxTemp}° /`;
+  }
+
+  function min() {
+    let minTemp = Math.round(props.data.temperature.minimum);
+    return `${minTemp}°`;
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="WeatherForecast-day">{day()}</div>
+      <img
+        src={props.data.condition.icon_url}
+        alt={props.data.condition.description}
+      />
+      <div className="WeatherForecast-temp">
+        <span className="WeatherForecast-temp-max">{max()}</span>
+        <span className="WeatherForecast-temp-min">{min()}</span>
+      </div>
+    </div>
+  );
+}
